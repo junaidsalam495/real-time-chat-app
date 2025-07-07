87f0c50ad9ebf0ec3c4297bd97956d78265ad721
junaidhnhtechsolutions <junaidhnhtechsolutions@gmail.com>
2025-07-07 17:07:08 +0500

    socket local url to railway based url

--- src/redux/createAPI.js
+++ src/redux/createAPI.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const chatAppAPI = createApi({
     reducerPath: "chatAppAPI",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://real-time-chat-app-eight-sooty.vercel.app/api/",
+        baseUrl: "https://real-time-chat-app-production-dc88.up.railway.app/api/",
         headers: { "Content-Type": "application/json" },
     }),
     endpoints: () => ({}),
